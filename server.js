45782920ada1f726e9a46de12883a0bd7e18ea1c
Karin Ohman <kaohma@gmail.com>
2019-02-12 17:38:41 -0700

    Add put and delete endpoints to backend server

--- server.js
+++ server.js
@@ -25,11 +25,15 @@ app.locals.notes = [
         completed: false
       }
     ]
+  },
+  {
+    id: 2,
+    title: "Hello",
+    issues: [{body: "beep", completed: true}],
   }
 ]
 
 app.listen(app.get("port"), () => {
-  console.log(app.locals.notes)
   console.log(`App is running on http://localhost:${app.get("port")}.`)
 })
 
@@ -52,4 +56,23 @@ app.get('/api/v1/notes/:id', (req, res) => {
   const note = app.locals.notes.find(note => note.id == req.params.id)
   if (!note) return res.status(404).json('Note not found')
   return res.status(200).json(note)
+})
+
+app.put('/api/v1/notes/:id', (req, res) => {
+  const { title, issues } = req.body
+  const { notes } = app.locals
+  if (!title || !issues) return res.status(422).json('Please provide a title and issues for your note')
+  const index = notes.findIndex(note => note.id == req.params.id)
+  if (index === -1) return res.status(404).json('Note not found')
+  const newNote = { id: notes[index].id, title, issues }
+  notes.splice(index, 1, newNote)
+  return res.status(200).json(newNote)
+})
+
+app.delete('/api/v1/notes/:id', (req, res) => {
+  const { notes } = app.locals
+  const index = notes.findIndex(note => note.id == req.params.id)
+  if (index === -1) return res.status(404).json('Note not found')
+  notes.splice(index, 1)
+  return res.status(200).json('Note successfully deleted')
 })
